54d086ab0227e695a64c46be8a0dc85cebf49b4f
HristoSimeonov1 <hrsimeonov.cybersec@yahoo.com>
2026-02-28 11:41:07 +0200

    Mad Libs game in C
    Implement a simple Mad Libs game that prompts for user input.

---
+++ c7.c
@@ -0,0 +1,38 @@
+// Mad libs game in C
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    char noun[50] = "";
+    char verb[50] = "";
+    char adjective1[50] = "";
+    char adjective2[50] = "";
+    char adjective3[50] = "";
+
+    printf("Enter an adjective (description): ");
+    fgets(adjective1, sizeof(adjective1), stdin);
+    adjective1[strlen(adjective1) - 1] = '\0';
+
+    printf("Enter a noun (animal / person): ");
+    fgets(noun, sizeof(noun), stdin);
+    noun[strlen(noun) - 1] = '\0';
+
+    printf("Enter an adjective (description): ");
+    fgets(adjective2, sizeof(adjective2), stdin);
+    adjective2[strlen(adjective2) - 1] = '\0';
+
+    printf("Enter a verb (ending with -ing): ");
+    fgets(verb, sizeof(verb), stdin);
+    verb[strlen(verb) - 1] = '\0';
+
+    printf("Enter an adjective (description): ");
+    fgets(adjective3, sizeof(adjective3), stdin);
+    adjective3[strlen(adjective3) - 1] = '\0';
+
+    printf("\nToday I went to a %s park and saw a %s %s %s.\n", adjective1, adjective2, noun, verb);
+    printf("It was a %s day!\n", adjective3);
+
+    return 0;
+}
